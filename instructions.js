a502bca72a6d151c3a73e70697b930764075fc02
mattholamieux <matthew.nelson@Matthews-MacBook-Pro.local>
2022-09-25 16:56:02 -0700

    modify instructions

--- instructions.js
+++ instructions.js
@@ -3,6 +3,6 @@ const instructions = [
     "space to start/stop",
     "mouse over tiles to play",
     "click/drag/release to record gestures",
-    "vertical scroll for effects",
     "right arrow for new image",
+    "scroll up for effects"
 ];

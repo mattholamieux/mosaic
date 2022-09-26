34394b117c4a62bc5f08826db612b3c497db37d8
mattholamieux <matthew.nelson@Matthews-MacBook-Pro.local>
2022-09-25 17:02:27 -0700

    update title

--- instructions.js
+++ instructions.js
@@ -1,7 +1,8 @@
 const instructions = [
-    "tile synth",
+    "mosaic",
     "space to start/stop",
     "mouse over tiles to play",
+    "synth params are mapped to rgb vals",
     "click/drag/release to record gestures",
     "right arrow for new image",
     "scroll up for effects"
